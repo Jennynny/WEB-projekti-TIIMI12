349d705edd6d99fbf5764784a46ec4ec750c9b9b
EnniKelloniemi <enni.kelloniemi@gmail.com>
2022-05-03 13:07:05 +0300

    Matikkavisan ulkoasun ja toiminnallisuuden muokkau

--- matikkavisa/scriptmatikka.js
+++ matikkavisa/scriptmatikka.js
@@ -30,7 +30,7 @@ function questionInput (number1, number2){
     answer.addEventListener("keypress", buttonCheckActivation); 
 }
 
-//Funktio tarkistaa onko tulos oikein ja syöttää sen näytölle. Laskee myös oikeat vastaukset 
+//Funktio tarkistaa onko tulos oikein ja syöttää sen näytölle. Jos kysymyksiä on kysytty 5, "näytä pisteet" nappula tulee esiin
 function checkAnswer(){
     let userAnswer = Number(document.getElementById("answer").value);
     console.log("Kysymys:" + questionNumber)
@@ -41,7 +41,7 @@ function checkAnswer(){
     else {
         document.getElementById("placeOfAnswer").innerHTML= "Vastasit väärin. Oikea vastaus on " + task+ ".";
     }
-    buttonNextDisabled()  //Voisiko nämä funktio yhdistää????
+    buttonNextDisabled() 
     buttonCheckDisabled()
     if (questionNumber == 5){
         console.log("hep")
@@ -55,36 +55,54 @@ function checkAnswer(){
     return score
 }
 
+//Katsoo kuinka monta pistettä on ja sen mukaan tulostaa kuvan näytölle. Piilottaa question boksin ja "näytä pistee" painikkeen.
 function showScore() {
-    document.getElementById("quesionview").style.display='none';
     const para = document.createElement("p");
-    if (score <= 1){
-        para.innerHTML = "Sait oikein " + score + "/5." + "<br>" + "Tarviset vielä harjoitusta"; 
+    const image = document.createElement("img");
+    //kuvien lähde: https://pixabay.com/fi/illustrations/kev%c3%a4t-tausta-m%c3%b6ykky-vihre%c3%a4-maali-1373475/ 
+    if (score == 0){
+        image.src= "images/loppukuva_pisteet0.jpg";
     }
-    else if (score <= 3){
-        para.innerHTML = "Sait oikein " +nscore + "/5." + "<br>" + "Hajoittele vielä vähän"; 
+    
+    else if (score == 1){
+        image.src= "images/loppukuva_pisteet1.jpg";
     }
-    else if (score <= 4){
-        para.innerHTML = "Sait oikein " + score + "/5." + "<br>" + "Hyvä!"; 
+    
+    else if (score == 2){
+        image.src= "images/loppukuva_pisteet2.jpg"; 
+
+    }
+    else if (score == 3){
+        image.src= "images/loppukuva_pisteet3.jpg";
+    }
+    else if (score == 4){
+        image.src= "images/loppukuva_pisteet4.jpg";
     }
     else{
-        para.innerHTML = "Loistavaa! Sait kaikki oikein!" + "<br>" + score + "/5."; 
+        image.src= "images/loppukuva_pisteet5.jpg";
     }
-    // Append to another element:
-    document.getElementById("scoreview").appendChild(para);
-    document.getElementById("scorebutton2").disabled = true;
+    document.getElementById("questionBox").hidden = true;
+    document.getElementById("scorebutton2").hidden = true;
+    //Lisätään elementti
+    document.getElementById("scoreview").appendChild(image);   
+    document.getElementById('buttondiv').style.display='block'; 
+}
+function homebuttonfunction(){
+
 }
 
 
 //PÄÄOHJELMA ALKAA TÄSTÄ
 
+//Määritellään muuttujat
 let number1 = 0;
 let number2 = 0;
 let task=0;
 let score = 0;
 let questionNumber=0;
 
-/*Aloitus sivu näkyvissä ja painaa aloitusnappia, niin kysymykset tulee esiin*/
+/*Aloitus sivu näkyvissä. Kun painaa aloitusnappia, niin kysymykset tulee esiin*/
+document.getElementById('buttondiv').style.display='none';
 document.getElementById('startview').style.display='block';
 document.getElementById("quesionview").style.display='none';
 document.getElementById("scorebutton").style.display='none';
@@ -95,7 +113,8 @@ document.getElementById("buttonstart").onclick=function(){
     reload()
 }
 
-
+//Funktio arpoo numerot, ja tulostaa arvotut numerot näytölle, jos kysymyksiä on kysytty alle viisi.
+//funktio aktivoituu, kun "seuraava" painiketta painetaan.
 function reload(){
     number1 = getRndInteger(1, 10)
     number2 = getRndInteger(1, 10)
@@ -109,9 +128,7 @@ function reload(){
         document.getElementById("placeOfAnswer").innerHTML="";
         questionInput (number1, number2)
     }
-    else{
-        document.getElementById("placeOfAnswer").innerHTML= "Visa on ohi";
-    }
+
     document.getElementById("answer").disabled =false;
     questionNumber++;
     document.getElementById("numberofquestion").innerHTML= "Kertolasku " + questionNumber + "/5";
